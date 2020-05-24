cd185b0e7791ee2f1c69983b0e7116cb2f2de047
Lucifermani <43871718+Lucifermani@users.noreply.github.com>
2020-05-24 15:18:26 +0530

    Add files via upload

---
+++ actions.js
@@ -0,0 +1,24 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "75%";
+  }
+  
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+// $(function (){
+//   $("#hidden").slice(0, 15).show();
+
+//   $("#loadmore").on('click', function(e)
+//   {
+//     e.preventDefault():
+
+//     $("#hidden:hidden").slice(0, 5).slideDown();
+//     if($("#hidden:hidden").length==0){
+//       $("#load").fadeOut('slow');
+//     }
+//     $('html, body').animate({
+//       scrollTop: $(this).offset().top
+//     }, 1500);
+//   });
+// });
